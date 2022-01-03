47ff7e518f340cba4fc9444edca89b62bb816aa3
dudulasry <dudu.lasry@viz.ai>
2022-01-04 00:06:33 +0200

    Add more layers at the end

--- src/models/fe.py
+++ src/models/fe.py
@@ -26,16 +26,24 @@ def freeze_model(model: torch.nn.Module):
         param.requires_grad = False
 
 
+def create_classifier(input_size, output_size):
+    return nn.Sequential(
+        nn.Linear(input_size, 1024),
+        nn.ReLU(),
+        nn.Linear(1024, output_size)
+    )
+
+
 def patch_backbone(backbone_name: str, backbone: str, output_size: int):
     if backbone_name == 'resnet18':
         input_size = backbone.fc.in_features
-        backbone.fc = nn.Linear(input_size, output_size)
+        backbone.fc = create_classifier(input_size, output_size)
     elif backbone_name == 'alexnet':
         input_size = backbone.classifier[6].in_features
-        backbone.classifier[6] = nn.Linear(input_size, output_size)
+        backbone.classifier[6] = create_classifier(input_size, output_size)
     elif backbone_name == 'vgg16':
         input_size = backbone.classifier[6].in_features
-        backbone.classifier[6] = nn.Linear(input_size, output_size)
+        backbone.classifier[6] = create_classifier(input_size, output_size)
     else:
         raise Exception(
             f'Could not find model with name {backbone_name} or it is not supported.')
@@ -51,69 +59,3 @@ def get_backbone_fn(backbone_name: str):
 
     raise Exception(
         f'Could not find model with name {backbone_name} or it is not supported.')
-    # elif backbone_name == 'squeezenet':
-    #     return models.squeezenet1_0
-    # elif backbone_name == 'densenet':
-    #     return models.densenet161
-    # elif backbone_name == 'inception':
-    #     return models.inception_v3
-    # elif backbone_name == 'googlenet':
-    #     return models.googlenet
-    # elif backbone_name == 'shufflenet':
-    #     return models.shufflenet_v2_x1_0
-    # elif backbone_name == 'mobilenet_v2':
-    #     return models.mobilenet_v2
-    # elif backbone_name == 'mobilenet_v3_large':
-    #     return models.mobilenet_v3_large
-    # elif backbone_name == 'mobilenet_v3_small':
-    #     return models.mobilenet_v3_small
-    # elif backbone_name == 'resnext50_32x4d':
-    #     return models.resnext50_32x4d
-    # elif backbone_name == 'wide_resnet50_2':
-    #     return models.wide_resnet50_2
-    # elif backbone_name == 'mnasnet':
-    #     return models.mnasnet1_0
-    # elif backbone_name == 'efficientnet_b0':
-    #     return models.efficientnet_b0
-    # elif backbone_name == 'efficientnet_b1':
-    #     return models.efficientnet_b1
-    # elif backbone_name == 'efficientnet_b2':
-    #     return models.efficientnet_b2
-    # elif backbone_name == 'efficientnet_b3':
-    #     return models.efficientnet_b3
-    # elif backbone_name == 'efficientnet_b4':
-    #     return models.efficientnet_b4
-    # elif backbone_name == 'efficientnet_b5':
-    #     return models.efficientnet_b5
-    # elif backbone_name == 'efficientnet_b6':
-    #     return models.efficientnet_b6
-    # elif backbone_name == 'efficientnet_b7':
-    #     return models.efficientnet_b7
-    # elif backbone_name == 'regnet_y_400mf':
-    #     return models.regnet_y_400mf
-    # elif backbone_name == 'regnet_y_800mf':
-    #     return models.regnet_y_800mf
-    # elif backbone_name == 'regnet_y_1_6gf':
-    #     return models.regnet_y_1_6gf
-    # elif backbone_name == 'regnet_y_3_2gf':
-    #     return models.regnet_y_3_2gf
-    # elif backbone_name == 'regnet_y_8gf':
-    #     return models.regnet_y_8gf
-    # elif backbone_name == 'regnet_y_16gf':
-    #     return models.regnet_y_16gf
-    # elif backbone_name == 'regnet_y_32gf':
-    #     return models.regnet_y_32gf
-    # elif backbone_name == 'regnet_x_400mf':
-    #     return models.regnet_x_400mf
-    # elif backbone_name == 'regnet_x_800mf':
-    #     return models.regnet_x_800mf
-    # elif backbone_name == 'regnet_x_1_6gf':
-    #     return models.regnet_x_1_6gf
-    # elif backbone_name == 'regnet_x_3_2gf':
-    #     return models.regnet_x_3_2gf
-    # elif backbone_name == 'regnet_x_8gf':
-    #     return models.regnet_x_8gf
-    # elif backbone_name == 'regnet_x_16gf':
-    #     return models.regnet_x_16gf
-    # elif backbone_name == 'regnet_x_32gf':
-    #     return models.regnet_x_32gf
